10a93ec932143e319a5ecc841f91dbd2e6bd4e68
ayule <68344743+ayule@users.noreply.github.com>
2023-03-31 14:21:22 -0600

    Add wait decorator

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -20,6 +20,27 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def get_item_input_box(self):
+        return self.browser.find_element_by_id("id_text")
+
+    def wait(fn):
+        def modified_fn(*args, **kwargs):
+            start_time = time.time()
+            while True:
+                try:
+                    return fn(*args, **kwargs)
+                except (AssertionError, WebDriverException) as e:
+                    if time.time() - start_time > MAX_WAIT:
+                        raise e
+                    time.sleep(0.5)
+
+        return modified_fn
+
+    @wait
+    def wait_for(self, fn):
+        return fn()
+
+    @wait
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
@@ -33,24 +54,13 @@ class FunctionalTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def wait_for(self, fn):
-        start_time = time.time()
-        while True:
-            try:
-                return fn()
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-
-    def get_item_input_box(self):
-        return self.browser.find_element_by_id("id_text")
-
+    @wait
     def wait_to_be_logged_in(self, email):
         self.wait_for(lambda: self.browser.find_element_by_link_text("Log out"))
         navbar = self.browser.find_element_by_css_selector(".navbar")
         self.assertIn(email, navbar.text)
 
+    @wait
     def wait_to_be_logged_out(self, email):
         self.wait_for(lambda: self.browser.find_element_by_name("email"))
         navbar = self.browser.find_element_by_css_selector(".navbar")
